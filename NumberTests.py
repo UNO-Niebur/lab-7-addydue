e64c11c9efd18bacd8f095b2eef82f30404b6a3d
addydue <10803506@nebraska.edu>
2026-03-09 02:02:24 +0000

    problem 3 done

--- NumberTests.py
+++ NumberTests.py
@@ -10,6 +10,12 @@ def isThreeOrFive(n):
 
 def isPrime(p):
   """Returns boolean (True/False) if the value given is prime."""
+  if p < 2:
+    return False
+
+  for i in range(2, p):
+    if p % i == 0:
+      return False
 
   return True
 
---
+++ Problem3.py
@@ -0,0 +1,43 @@
+#Problem3.py
+#Project Euler problem 3
+#Addy Duering
+#3/8/26
+#Lab 7
+# My approach is to find all prime factors and return the largest one.
+
+from NumberTests import isPrime
+
+def find_largest_prime_factor(n):
+    """Find the largest prime factor of n."""
+    largest_prime = 1
+    
+    # Check for 2
+    if n % 2 == 0:
+        if isPrime(2):
+            largest_prime = 2
+        while n % 2 == 0:
+            n //= 2
+    
+    # Check odd factors
+    i = 3
+    while i * i <= n:
+        if n % i == 0:
+            if isPrime(i):
+                largest_prime = i
+            while n % i == 0:
+                n //= i
+        i += 2
+    
+    # If n is a prime number greater than 2
+    if n > 1 and isPrime(n):
+        largest_prime = n
+    
+    return largest_prime
+
+def main():
+    number = 600851475143
+    result = find_largest_prime_factor(number)
+    print(f"The largest prime factor of {number} is {result}")
+
+if __name__ == '__main__':
+    main()
